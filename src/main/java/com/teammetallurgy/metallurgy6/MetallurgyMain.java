cebb76f458b10a6367e4be4f65343c62f3f66e2c
Vequinox <josephrogerahrens@gmail.com>
2020-06-24 13:19:51 -0500

    Updated mods.toml, build.gradle, and began work on setup

--- src/main/java/com/teammetallurgy/metallurgy6/MetallurgyMain.java
+++ src/main/java/com/teammetallurgy/metallurgy6/MetallurgyMain.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.teammetallurgy.metallurgy6;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,28 +19,23 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
-{
+@Mod("metallurgy6")
+public class MetallurgyMain {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "metallurgy6";
 
-    public ExampleMod() {
-        // Register the setup method for modloading
+    public MetallurgyMain() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
@@ -51,14 +46,12 @@ public class ExampleMod
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
     }
 
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
+    private void enqueueIMC(final InterModEnqueueEvent event) {
         // some example code to dispatch IMC to another mod
         InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
-    private void processIMC(final InterModProcessEvent event)
-    {
+    private void processIMC(final InterModProcessEvent event) {
         // some example code to receive and process InterModComms from other mods
         LOGGER.info("Got IMC {}", event.getIMCStream().
                 map(m->m.getMessageSupplier().get()).
---
+++ src/main/java/com/teammetallurgy/metallurgy6/init/MetallurgyOres.java
@@ -0,0 +1,11 @@
+package com.teammetallurgy.metallurgy6.init;
+
+import com.teammetallurgy.metallurgy6.MetallurgyMain;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ObjectHolder;
+
+@ObjectHolder(MetallurgyMain.MOD_ID)
+@Mod.EventBusSubscriber(modid = MetallurgyMain.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class MetallurgyOres {
+
+}
