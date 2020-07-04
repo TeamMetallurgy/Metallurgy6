07055c8d65ac1081b3822a0ea0b84cfc1090166f
Vequinox <josephrogerahrens@gmail.com>
2020-07-04 11:34:24 -0500

    Created scripts for generating ingots

---
+++ scripts/ItemGen/ingot/GenerateIngots.ps1
@@ -0,0 +1,21 @@
+﻿$data = Get-Content -Path data.txt
+$registry = Get-Content -Path ingotregistry.txt
+$model = Get-Content -Path ingotmodel.txt
+$lang = Get-Content -Path ingotlang.txt
+
+foreach($d in $data){
+    $lcApiName = $d.ToLower().Replace(' ', '_').Replace("'", '')
+    $ucApiName = $d.ToUpper().Replace(' ', '_').Replace("'", '')
+
+    $ingotregistry = $registry.Replace('{API_NAME}', $ucApiName).Replace('{api_name}', $lcApiName)
+    $ingotregistry >> "Output\Registry.txt"
+    #$ingotregistry
+
+    $ingotmodel = $model.Replace('{api_name}', $lcApiName)
+    $filename = $lcApiName + '_ingot'
+    #$ingotmodel | Out-File "Output\blockmodel\$filename.json" -Encoding utf8
+
+    $ingotlang = $lang.Replace('{api_name}', $lcApiName).Replace('{Api_name}', $d)
+    $ingotlang >> "Output\Lang.txt"
+    #$ingotlang
+}
