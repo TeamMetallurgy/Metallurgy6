f0776b0b6136bcd037e4d857c2ab78eb21e0f62d
Vequinox <josephrogerahrens@gmail.com>
2020-07-04 11:35:04 -0500

    Created ingots for the given materials

--- src/main/java/com/teammetallurgy/metallurgy6/objects/items/MetallurgyItems.java
+++ src/main/java/com/teammetallurgy/metallurgy6/objects/items/MetallurgyItems.java
@@ -204,6 +204,42 @@ public class MetallurgyItems {
     public static final Item PURE_STEEL_SWORD = register("pure_steel_sword", new SwordItem(MetallurgyItemTier.PURE_STEEL, 3, -2.4F, new Item.Properties().group(MetallurgyMain.GROUP)));
     public static final Item PURE_STEEL_HOE = register("pure_steel_hoe", new HoeItem(MetallurgyItemTier.PURE_STEEL, 0.0F, new Item.Properties().group(MetallurgyMain.GROUP)));
 
+    public static final Item SLAG_INGOT = register("slag_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item COPPER_INGOT = register("copper_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item TIN_INGOT = register("tin_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item BRONZE_INGOT = register("bronze_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item LEAD_INGOT = register("lead_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item MYCELITE_INGOT = register("mycelite_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item THOKCHA_INGOT = register("thokcha_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item NICKEL_INGOT = register("nickel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item CUPRONICKEL_INGOT = register("cupronickel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item MONEL_INGOT = register("monel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item CROWN_GOLD_INGOT = register("crown_gold_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item WHITE_GOLD_INGOT = register("white_gold_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item PEWTER_INGOT = register("pewter_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item MOLYBDOCHALKOS_INGOT = register("molybdochalkos_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item HEPATIZON_INGOT = register("hepatizon_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item ANGMALLEN_INGOT = register("angmallen_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item MANGANESE_INGOT = register("manganese_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item HEAVY_STEEL_INGOT = register("heavy_steel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item DAMASCUS_STEEL_INGOT = register("damascus_steel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item MANGANESE_STEEL_INGOT = register("manganese_steel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item NICKEL_STEEL_INGOT = register("nickel_steel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item FUNGAL_STEEL_INGOT = register("fungal_steel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item METEOR_STEEL_INGOT = register("meteor_steel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item WOVEN_STEEL_INGOT = register("woven_steel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item ZINC_INGOT = register("zinc_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item BRASS_INGOT = register("brass_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item SILVER_INGOT = register("silver_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item PLATINUM_INGOT = register("platinum_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item ROSE_GOLD_INGOT = register("rose_gold_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item ELECTRUM_INGOT = register("electrum_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item ARGENTAN_INGOT = register("argentan_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item RHODITE_INGOT = register("rhodite_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item CORINTHIAN_STEEL_INGOT = register("corinthian_steel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item SHIMMER_STEEL_INGOT = register("shimmer_steel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item KINGS_STEEL_INGOT = register("kings_steel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
+    public static final Item PURE_STEEL_INGOT = register("pure_steel_ingot", new Item((new Item.Properties()).group(MetallurgyMain.GROUP)));
 
     private static Item register(String registryName, Item thisItem){
         thisItem.setRegistryName(registryName);
--- src/main/java/com/teammetallurgy/metallurgy6/util/MetallurgyItemTier.java
+++ src/main/java/com/teammetallurgy/metallurgy6/util/MetallurgyItemTier.java
@@ -13,112 +13,112 @@ public enum MetallurgyItemTier implements IItemTier {
         return Ingredient.fromItems(MetallurgyOres.TEST_ORE);
     }),
     SLAG(2, 300, 5.0F, 0.0F, 6, () -> {
-        return Ingredient.fromTag(MetallurgyItems.SLAG_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.SLAG_INGOT);
     }),
     COPPER(2, 180, 5.0F, 1.0F, 7, () -> {
-        return Ingredient.fromTag(MetallurgyItems.COPPER_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.COPPER_INGOT);
     }),
     TIN(2, 132, 5.0F, 1.0F, 9, () -> {
-        return Ingredient.fromTag(MetallurgyItems.TIN_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.TIN_INGOT);
     }),
     BRONZE(3, 250, 6.0F, 2.0F, 15, () -> {
-        return Ingredient.fromTag(MetallurgyItems.BRONZE_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.BRONZE_INGOT);
     }),
     LEAD(3, 500, 4.0F, 2.0F, 10, () -> {
-        return Ingredient.fromTag(MetallurgyItems.LEAD_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.LEAD_INGOT);
     }),
     MYCELITE(3, 150, 5.0F, 2.0F, 16, () -> {
-        return Ingredient.fromTag(MetallurgyItems.MYCELITE_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.MYCELITE_INGOT);
     }),
     THOKCHA(3, 200, 6.0F, 2.0F, 16, () -> {
-        return Ingredient.fromTag(MetallurgyItems.THOKCHA_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.THOKCHA_INGOT);
     }),
     NICKEL(3, 250, 8.0F, 2.0F, 12, () -> {
-        return Ingredient.fromTag(MetallurgyItems.NICKEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.NICKEL_INGOT);
     }),
     CUPRONICKEL(3, 350, 8.0F, 2.0F, 14, () -> {
-        return Ingredient.fromTag(MetallurgyItems.CUPRONICKEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.CUPRONICKEL_INGOT);
     }),
     MONEL(3, 500, 9.0F, 2.0F, 16, () -> {
-        return Ingredient.fromTag(MetallurgyItems.MONEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.MONEL_INGOT);
     }),
     CROWN_GOLD(3, 225, 12.0F, 2.0F, 22, () -> {
-        return Ingredient.fromTag(MetallurgyItems.CROWN_GOLD_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.CROWN_GOLD_INGOT);
     }),
     WHITE_GOLD(3, 300, 13.0F, 2.0F, 26, () -> {
-        return Ingredient.fromTag(MetallurgyItems.WHITE_GOLD_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.WHITE_GOLD_INGOT);
     }),
     PEWTER(3, 850, 10.0F, 2.0F, 12, () -> {
-        return Ingredient.fromTag(MetallurgyItems.PEWTER_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.PEWTER_INGOT);
     }),
     MOLYBDOCHALKOS(3, 700, 10.0F, 2.0F, 14, () -> {
-        return Ingredient.fromTag(MetallurgyItems.MOLYBDOCHALKOS_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.MOLYBDOCHALKOS_INGOT);
     }),
     HEPATIZON(3, 300, 14.0F, 3.0F, 28, () -> {
-        return Ingredient.fromTag(MetallurgyItems.HEPATIZON_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.HEPATIZON_INGOT);
     }),
     ANGMALLEN(3, 350, 14.0F, 3.0F, 28, () -> {
-        return Ingredient.fromTag(MetallurgyItems.ANGMALLEN_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.ANGMALLEN_INGOT);
     }),
     MANGANESE(3, 250, 10.0F, 2.0F, 18, () -> {
-        return Ingredient.fromTag(MetallurgyItems.MANGANESE_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.MANGANESE_INGOT);
     }),
     HEAVY_STEEL(4, 1800, 10.0F, 3.0F, 12, () -> {
-        return Ingredient.fromTag(MetallurgyItems.HEAVY_STEEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.HEAVY_STEEL_INGOT);
     }),
     DAMASCUS_STEEL(4, 1600, 12.0F, 3.0F, 19, () -> {
-        return Ingredient.fromTag(MetallurgyItems.DAMASCUS_STEEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.DAMASCUS_STEEL_INGOT);
     }),
     MANGANESE_STEEL(4, 1750, 12.0F, 3.0F, 18, () -> {
-        return Ingredient.fromTag(MetallurgyItems.MANGANESE_STEEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.MANGANESE_STEEL_INGOT);
     }),
     NICKEL_STEEL(4, 1450, 12.0F, 3.0F, 16, () -> {
-        return Ingredient.fromTag(MetallurgyItems.NICKEL_STEEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.NICKEL_STEEL_INGOT);
     }),
     FUNGAL_STEEL(4, 1250, 12.0F, 3.0F, 20, () -> {
-        return Ingredient.fromTag(MetallurgyItems.FUNGAL_STEEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.FUNGAL_STEEL_INGOT);
     }),
     METEOR_STEEL(4, 1250, 12.0F, 3.0F, 20, () -> {
-        return Ingredient.fromTag(MetallurgyItems.METEOR_STEEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.METEOR_STEEL_INGOT);
     }),
     WOVEN_STEEL(5, 3000, 16.0F, 4.0F, 22, () -> {
-        return Ingredient.fromTag(MetallurgyItems.WOVEN_STEEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.WOVEN_STEEL_INGOT);
     }),
     ZINC(2, 35, 7.0F, 0.0F, 20, () -> {
-        return Ingredient.fromTag(MetallurgyItems.ZINC_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.ZINC_INGOT);
     }),
     BRASS(3, 225, 8.0F, 0.0F, 16, () -> {
-        return Ingredient.fromTag(MetallurgyItems.BRASS_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.BRASS_INGOT);
     }),
     SILVER(3, 33, 10.0F, 0.0F, 24, () -> {
-        return Ingredient.fromTag(MetallurgyItems.SILVER_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.SILVER_INGOT);
     }),
     PLATINUM(3, 55, 14.0F, 0.0F, 20, () -> {
-        return Ingredient.fromTag(MetallurgyItems.PLATINUM_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.PLATINUM_INGOT);
     }),
     ROSE_GOLD(3, 200, 12.0F, 1.0F, 22, () -> {
-        return Ingredient.fromTag(MetallurgyItems.ROSE_GOLD_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.ROSE_GOLD_INGOT);
     }),
     ELECTRUM(3, 100, 13.0F, 1.0F, 26, () -> {
-        return Ingredient.fromTag(MetallurgyItems.ELECTRUM_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.ELECTRUM_INGOT);
     }),
     ARGENTAN(3, 300, 14.0F, 2.0F, 18, () -> {
-        return Ingredient.fromTag(MetallurgyItems.ARGENTAN_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.ARGENTAN_INGOT);
     }),
     RHODITE(3, 100, 14.0F, 2.0F, 28, () -> {
-        return Ingredient.fromTag(MetallurgyItems.RHODITE_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.RHODITE_INGOT);
     }),
     CORINTHIAN_STEEL(4, 500, 15.0F, 3.0F, 30, () -> {
-        return Ingredient.fromTag(MetallurgyItems.CORINTHIAN_STEEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.CORINTHIAN_STEEL_INGOT);
     }),
     SHIMMER_STEEL(4, 450, 15.0F, 3.0F, 34, () -> {
-        return Ingredient.fromTag(MetallurgyItems.SHIMMER_STEEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.SHIMMER_STEEL_INGOT);
     }),
     KINGS_STEEL(5, 2000, 16.0F, 4.0F, 32, () -> {
-        return Ingredient.fromTag(MetallurgyItems.KINGS_STEEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.KINGS_STEEL_INGOT);
     }),
     PURE_STEEL(6, 5000, 17.0F, 5.0F, 34, () -> {
-        return Ingredient.fromTag(MetallurgyItems.PURE_STEEL_INGOT);
+        return Ingredient.fromItems(MetallurgyItems.PURE_STEEL_INGOT);
     });
 
     private final int harvestLevel;
