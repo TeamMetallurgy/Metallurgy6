a38cabb6f66be93cdbe74cb8e249623df1671af3
Vequinox <josephrogerahrens@gmail.com>
2020-07-05 17:57:48 -0500

    Added scripts for generating dusts, nuggets, and crushed ores

---
+++ scripts/ItemGen/dust/GenerateDusts.ps1
@@ -0,0 +1,26 @@
+﻿$data = Get-Content -Path data.txt
+$registry = Get-Content -Path dustregistry.txt
+$model = Get-Content -Path dustmodel.txt
+$tinymodel = Get-Content -Path tinydustmodel.txt
+$lang = Get-Content -Path dustlang.txt
+
+foreach($d in $data){
+    $lcApiName = $d.ToLower().Replace(' ', '_').Replace("'", '')
+    $ucApiName = $d.ToUpper().Replace(' ', '_').Replace("'", '')
+
+    $dustregistry = $registry.Replace('{API_NAME}', $ucApiName).Replace('{api_name}', $lcApiName)
+    $dustregistry >> "Output\Registry.txt"
+    #$dustregistry
+
+    $dustmodel = $model.Replace('{api_name}', $lcApiName)
+    $filename = $lcApiName + '_dust'
+    $dustmodel | Out-File "Output\blockmodel\$filename.json" -Encoding utf8
+    
+    $tinydustmodel = $tinymodel.Replace('{api_name}', $lcApiName)
+    $tinyfilename = 'tiny_' + $lcApiName + '_dust'
+    $tinydustmodel | Out-File "Output\blockmodel\$tinyfilename.json" -Encoding utf8
+
+    $dustLang = $lang.Replace('{api_name}', $lcApiName).Replace('{Api_name}', $d)
+    $dustLang >> "Output\Lang.txt"
+    #$dustLang
+}
---
+++ scripts/ItemGen/nugget_and_crushed/GeneratedNuggetsAndCrushedOres.ps1
@@ -0,0 +1,26 @@
+﻿$data = Get-Content -Path data.txt
+$registry = Get-Content -Path nuggetandcrushedoreregistry.txt
+$nuggetmodel = Get-Content -Path nuggetmodel.txt
+$crushedmodel = Get-Content -Path crushedoremodel.txt
+$lang = Get-Content -Path nuggetandcrushedorelang.txt
+
+foreach($d in $data){
+    $lcApiName = $d.ToLower().Replace(' ', '_').Replace("'", '')
+    $ucApiName = $d.ToUpper().Replace(' ', '_').Replace("'", '')
+
+    $itemregistry = $registry.Replace('{API_NAME}', $ucApiName).Replace('{api_name}', $lcApiName)
+    $itemregistry >> "Output\Registry.txt"
+    #$itemregistry
+
+    $nmodel = $nuggetmodel.Replace('{api_name}', $lcApiName)
+    $nuggetfilename = $lcApiName + '_nugget'
+    $nmodel | Out-File "Output\blockmodel\$nuggetfilename.json" -Encoding utf8
+    
+    $cmodel = $crushedmodel.Replace('{api_name}', $lcApiName)
+    $crushedfilename = 'crushed_' + $lcApiName
+    $cmodel | Out-File "Output\blockmodel\$crushedfilename.json" -Encoding utf8
+
+    $itemlang = $lang.Replace('{api_name}', $lcApiName).Replace('{Api_name}', $d)
+    $itemlang >> "Output\Lang.txt"
+    #$itemlang
+}
