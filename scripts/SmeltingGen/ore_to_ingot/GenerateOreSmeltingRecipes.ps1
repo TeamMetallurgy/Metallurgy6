d9c1e5e641f6084bbfaef891a8b5bcd9f573245b
Vequinox <josephrogerahrens@gmail.com>
2020-07-13 23:25:12 -0500

    Changed lang file; Added smelting recipes

---
+++ scripts/SmeltingGen/ore_to_ingot/GenerateOreSmeltingRecipes.ps1
@@ -0,0 +1,12 @@
+﻿$data = Get-Content -Path data.txt
+$template = Get-Content -Path smeltingtemplate.txt
+
+foreach($d in $data){
+    $name,$exp = $td -split '\t'
+
+    $lcApiName = $name.ToLower().Replace(' ', '_').Replace("'", '')
+
+    $recipe = $template.Replace('{api_name}', $lcApiName).Replace('{exp}', $exp)
+    #$recipe | Out-File $lcApiName+'_ingot.json' -Encoding utf8
+    $recipe
+}
